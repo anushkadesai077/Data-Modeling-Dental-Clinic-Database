e85b2f6abfc7d4b8aeab2982e799fb417cfe9947
Anushka Desai <93955872+anushkadesai077@users.noreply.github.com>
2022-03-11 16:13:43 -0500

    Add files via upload

---
+++ EmployeeClusterInsertQuery.sql
@@ -0,0 +1,78 @@
+-- Employee Entity
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'9572508368','Molly','Weasley','107 Cactus Rd','02115','6676461305','m.weasley@gmail.com','9/29/92','Female',0);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'2659463693','Amy','Dunne','43 Marchall St','36104','6676461306','amydone@yahoo.com','8/29/80','Female',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'9344345839','Claire','Smith','8919 4th St','99801','6676461307','claire.s@gmail.com','7/29/75','Female',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'2694866251','Madeline','James','1185 Boylston St','85001','6676461308','mjms@gmail.com','6/25/69','Female',0);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'3704480461','Kateline','Williams','81 Ann Dr.','78701','6676461309','k.williams@gmail.com','6/26/69','Female',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'6245940951','Emma','Miller','25 Allied Dr.','95814','6676461310','emmam@gmail.com','6/27/69','Female',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'6247318855','Jada','Hawthorne','13 Green St','29217','6676461311','jadah@gmail.com','6/28/69','Female',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'5774154611','Ang ','Lee','83 NW','06103','6676461312','anglee@gmail.com','6/29/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'5269224769','Willow','Smith','1 West Saxon St','65101','6676461313','smithw@gmail.com','6/30/69','Female',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'7145978215','Jake','Lowe','533 Penn Drive','59623','6676461314','jlowe@gmail.com','7/1/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'9773763805','Connor','Bridges','96 Hanover Ave','68502','6676461315','connb@gmail.com','7/2/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'6719313914','Wyatt','Roy','312 Colonial St','96813','6676461316','roy.w@gmail.com','7/3/69','Male',0);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'3987222630','Cody','Waldorf','8 Rockaway Ave','08608','6676461317','waldrof@gmail.com','7/4/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'8191392298','Edward ','Yang','2 Corona Lane','87501','6676461318','yang.ew@gmail.com','7/5/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'6788408207','Andrew','Lau','72 Arlington Ave','12207','6676461319','andy.lau@gmail.com','7/6/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'4742583976','Wong-Kar','Wai','369 Sunset Ave','27601','6676461320','wkwai@gmail.com','7/7/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'6100056248','Agnes','Varda','1 Surrey Dr.','72201','6676461321','agnesv@gmail.com','7/8/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'5095895478','Jean-Luc','Godard','8 Princeton Ave','80202','6676461322','jlgod@gmail.com','7/9/69','Male',0);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'6138904402','Xavier','Dolan','17 Sherwood Court','19901','6676461323','xavierdolan@gmail.com','7/10/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (2,'6672986426','David','Fincher','36 Hillcrest St','33203','6676461324','dafincher@gmail.com','7/11/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'7550178404','Martin','Scorcese','176 Bonney Lake','32301','6676461325','m.scorcese@gmail.com','7/12/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (2,'6237684945','Ridley','Scott','19 Schilling Dr.','30303','6676461326','ridles@gmail.com','7/13/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'336394431','Greta','Gerwig','2017 Bates Ave','40601','6676461327','gerwig.greta@yahoo.com','7/14/69','Female',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (2,'7978978404','Sofia','Coppola','201 Monroe Road','70802','6676461328','sofia.coppola@gmail.com','7/15/69','Female',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'6476201477','Wes','Anderson','2 E Smythe Rd','04330','6676461329','anderson.w@gmail.com','7/16/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (2,'8476208477','Ingmar','Bergman','724 Alder St','21401','6676461330','bergmanin@gmail.com','7/17/69','Male',1);
+INSERT INTO EMPLOYEES (EmployeeTypeId,NPID,FirstName,LastName,AddressFirstLine,ZipCode,PhoneNo,Email,DOB,Gender,isWorking)
+VALUES (1,'5476201745','Eric','Rohmer','6100 Carillon Pt','17101','6676461331','eric.rohmer@gmail.com','7/18/69','Male',1);
+
+USE MindYourOwnMolars
+
+-- Employee Types
+INSERT INTO EmployeeTypes (Job_Title) VALUES ('Dentist');
+INSERT INTO EmployeeTypes (Job_Title) VALUES ('Hygienist');
+
+
+-- Employee Specializations
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (1,'Endodontist');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (1,'Orthodontist');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (1,'Oral Surgeon');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (3,'Pedodontist');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (3,'Orthodontist');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (7,'Anesthisiologists');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (7,'Oral Surgeon');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (8,'Periodontist');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (9,'Orthodontist');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (10,'Orthodontist');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (10,'Prosthodontist');
+INSERT INTO EmployeeSpecializations (EmployeeId,Specializations) VALUES (11,'Endodontist');
+
+select * from EmployeeSpecializations
