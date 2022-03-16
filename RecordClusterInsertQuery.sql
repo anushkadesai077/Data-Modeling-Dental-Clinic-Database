93ffdddec0a9b74afffa99e646e3df62c1c72f92
Anushka Desai <93955872+anushkadesai077@users.noreply.github.com>
2022-03-16 08:12:54 -0400

    Add files via upload

---
+++ RecordClusterInsertQuery.sql
@@ -0,0 +1,124 @@
+-- InsuranceInfo Entity
+Use MindYourOwnMolars
+
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('6758 7654-A',12,'Gary','Nguyen','MaxxHealth','12/1/25')
+
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('3458 7154-A',13,'Mary','Baker','MaxxHealth ','1/31/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('9958 7100-C',14,'Susan','Hall','MaxxHealth ','3/31/23')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('2009 1190-B',15,'Lisa','Campbell','AllState','1/1/23')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('6909 1191-B',16,'Ashley','Turner','Amica Insurance ','3/1/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('6609 4590-C',6,'Joshua','Thomas','AllState','5/20/20')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('2088 6190-A',7,'George','Taylor','Progressive','1/15/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('2009 1191-A',8,'Edward','Lee','MaxxHealth ','1/1/24')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('4449 4490-B',9,'Jason','Thompson','AllState','4/15/24')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('2709 6945-D',10,'Ryan','White','Amica Insurance ','1/1/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('2909 2245-D',11,'Jacob','Scott','Amica Insurance ','5/1/21')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('5509 6995-D',1,'Mark','Smith','AllState','10/21/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('9709 6965-D',2,'Anthony','Rodriguez','Nationwide','1/1/25')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('7709 6885-A',3,'Donald','Brown','Progressive','1/1/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('0119 3645-B',4,'Steve','Davis','Amica Insurance ','11/30/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('0009 0945-B',5,'Andrew','Wilson','Nationwide','9/11/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('0709 6885-C',17,'Laura','Parker','Progressive','5/24/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('5709 6885-C',17,'Laura','Parker','Progressive','5/24/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('8809 0085-A',18,'Amy','Collins','Aetna','8/14/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('9709 0005-B',19,'Shirley','Stewart','Cigna','12/3/22')
+INSERT INTO INSURANCEINFO (InsuranceId,PatientId,HolderFirstName,HolderLastName,InsuranceCompany,MaxCoverage) VALUES ('1709 3335-D',20,'Debra','Cooper','Aetna','7/10/22')
+
+
+use MindYourOwnMolars
+-- ALLERGIES ENTITY
+select * from 
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (12,'12/1/21',0,1,1,1,1,0,'celery')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (13,'12/12/21',0,1,1,1,1,0,'Peanuts, eggs')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (14,'11/23/21',0,0,0,1,0,0,'Peaches')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (15,'12/1/21',0,0,0,1,0,1,'Eggs')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (16,'10/12/21',1,0,0,0,1,1,'milk')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (6,'7/19/21',1,0,0,0,0,1,'Peanuts ')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (7,'10/31/21',1,1,1,0,0,1,'soybeans')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (8,'12/12/21',0,1,1,0,0,0,'Peaches')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (9,'12/30/20',0,1,1,0,0,0,'Wheat')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (10,'10/12/21',0,0,0,0,1,0,'milk')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (11,'12/1/21',0,0,0,1,1,0,'Peanuts ')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (1,'10/12/21',0,0,0,1,0,0,'Kiwi')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (2,'8/1/21',0,0,0,1,0,1,'Peaches, celery')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (3,'11/23/21',1,0,0,0,0,1,'sesame')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (4,'11/15/21',1,0,0,1,1,0,'Kiwi')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (5,'10/25/21',0,1,1,0,1,1,'Peanuts, milk')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (17,'9/18/21',0,1,1,1,1,0,'mustard')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (18,'12/1/21',1,1,1,0,1,1,'oysters')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (19,'11/17/21',1,0,0,0,0,0,'Kiwi')
+INSERT INTO ALLERGIES (PatientId ,CheckinDate ,Antibiotic ,Aspirin ,Codeine ,Darvon ,Local_Anesthetic,Nitrous_Oxide,Others) VALUES (20,'12/1/21',0,0,0,0,0,0,'Mustard')
+select * from allergies
+
+
+-- DentalRecords Entity
+
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (1,1,'Rot on Left incisor ','Conduct removal on the tooth',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (2,2,'Rot on maxillary first molar tooth','Perform dental filling in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (4,3,'Bleeding on left gum due to wisdom tooth','Check if pain persists in next appointment ',0);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (5,4,'Rot on maxillary second molar','Perform a root canal treatment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (6,5,'No cavity found','Performed general cleaning',0);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (7,6,'Bleeding on left incisor','Perform dental filling in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (14,7,'No cavity found','Performed general cleaning',0);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (17,8,'No cavity found','Performed general cleaning',0);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (1,9,'Infection on central incisor ','Performed cleaning, root canal in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (4,10,'Bleeding on right gum due to wisdom tooth','Check if pain persists in next appointment ',0);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (6,11,'Rot on Left incisor ','Perform dental filling in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (3,12,'Infection on maxillary third molar','Root canal to be performed in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (10,13,'No cavity found','Performed general cleaning',0);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (2,14,'Rot on left canine','Perform dental filling in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (6,15,'No cavity found','Performed general cleaning',0);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (9,16,'Rot on left canine tooth','Perform dental filling in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (1,21,'Infection on first  premolar','Root canal to be performed in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (19,22,'Rot on Left incisor ','Perform dental filling in next appointment',1);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (11,19,'No cavity found','Performed general cleaning',0);
+INSERT INTO DentalRecords (PatientId ,AppointmentId ,Findings ,Comments ,ProcedureRequired) VALUES (16,20,'No cavity found','Performed general cleaning',0);
+
+select * from Appointments
+-- Procedures Entity
+
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Root canal treatment','Requires more work next week',2,2,1)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental cavity filling','Second appointment is needed',1,3,5)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Tooth extraction','X-ray is needed for first appointment',11,4,2)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental crown procedure','Requires more work next week',10,7,4)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental implants','Final appointment is needed ',6,6,3)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Braces','Requires next appointment',1,15,7)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental cavity filling','Requires more work next week',5,21,9)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Root canal treatment','X-ray is needed for first appointment',12,19,10)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental implants','Requires next appointment',15,20,8)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental cavity filling','Requires next appointment',4,15,11)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Root canal treatment','Requires more work next week',5,16,15)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental crown procedure','X-ray is needed for first appointment',19,9,18)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental implants','Final appointment is needed ',17,19,12)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Braces','Requires next appointment',5,10,19)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental cavity filling','Requires more work next week',1,1,13)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental cavity filling','Requires next appointment',5,5,6)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Root canal treatment','X-ray is needed for first appointment',9,1,21)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental crown procedure','Final appointment is needed ',15,13,23)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Dental implants','X-ray is needed for first appointment',2,11,24)
+INSERT INTO Procedures (ProcedurePerformed ,ProcedureComments ,ToothAssociated ,AppointmentId ,PatientId) VALUES ('Braces','Requires next appointment',12,12,14)
+
+-- Prescriptions Entity
+
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (2,1)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (5,8)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (8,6)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (15,8)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (1,5)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (10,2)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (14,9)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (9,6)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (16,1)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (3,9)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (11,7)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (22,3)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (6,10)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (21,10)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (4,7)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (12,5)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (20,11)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (19,4)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (13,12)
+INSERT INTO Prescriptions (AppointmentId ,DrugId) VALUES (7,7)
+
+
